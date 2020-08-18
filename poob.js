2f2dffd2d470df6f18eebe4e6450fedf2a2cc128
Maybegus <maybegus@gmail.com>
2020-08-17 22:25:56 -0600

    commit for the night

--- poob.js
+++ poob.js
@@ -13,14 +13,17 @@ function network(neurons, nodes) {
   this.nodes = nodes
 };
 
-function generate(amount, inputs, outputs, maxlayer, maxheight) {
+function generate(amount, inputs, outputs, maxlayer, maxheight, minlayer, minheight) {
   let cluster = [];
-  for (i = 0; i < maxlayer; i++) {
-    console.log("it works!")
-  }
+  let stagingneurons = [];
   for (i = 0; i < amount; i++) {
+    for (i = 0; i < maxlayer; i++) {
+      for (i = 0; i < maxheight; i++) {
+        Math.random()
+      }
+    }
     //cluster[i] = new network([new neuron([1, 3], Math.random() * 2, 1), new neuron([1, 2], Math.random() * 2, 1)], new node(1, "yeet", 1))
-    //cluster[i] = new network()
+    cluster[i] = new network()
   }
   console.log(cluster);
 }
